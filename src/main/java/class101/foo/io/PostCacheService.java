bda5768ddb0513d0538e309d13633b64d2deb716
yundevk <yundevk@gmail.com>
2021-07-30 22:11:25 +0900

    pageCache schedule 추가

--- src/main/java/class101/foo/io/PostCacheService.java
+++ src/main/java/class101/foo/io/PostCacheService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +15,7 @@ public class PostCacheService {
 
     private Page<Post> firstPostPage;
 
+    @Scheduled(cron = "* * * * * *")
     public void updateFirstPostPage() {
         firstPostPage = postRepository.findAll(
                 PageRequest.of(0, 20, Sort.by("id").descending())
